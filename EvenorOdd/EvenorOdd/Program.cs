0b09748718780a9a0aeb4428d9b51011292e8b23
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-09-22 10:17:53 +0530

    Even Odd Program in C#

---
+++ EvenorOdd/EvenorOdd/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenorOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number \n");
+            int no = int.Parse(Console.ReadLine());
+            if (no % 2 == 0)
+            {
+                Console.WriteLine("Even Number");
+            }
+            else 
+            {
+                Console.WriteLine("Odd Number ");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
