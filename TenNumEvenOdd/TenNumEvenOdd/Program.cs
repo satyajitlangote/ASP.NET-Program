1dc623774c073c4def561d9934ec97a7de8b7a25
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-09-22 10:14:57 +0530

    Ten number even odd program in C Sharp

---
+++ TenNumEvenOdd/TenNumEvenOdd/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenNumEvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int no, Ecnt = 0, Ocnt = 0, Tcnt=0;
+            Console.WriteLine("Enter the number \n ");
+            for (int i = 0; i <= 10; i++)
+            {
+                no = int.Parse(Console.ReadLine());
+                if (no == 0)
+                {
+                    Tcnt++;
+                }
+                else
+                {
+                    if (no % 2 == 0)
+                    {
+                        Ecnt++;
+                    }
+                    else
+                    {
+                        Ocnt++;
+                    }
+                }
+                Console.WriteLine("\n Even Count \n ="+Ecnt+"\n Odd Count \n"+Ocnt+"2 even or odd count \n "+Tcnt);
+            }
+
+        }
+    }
+}
