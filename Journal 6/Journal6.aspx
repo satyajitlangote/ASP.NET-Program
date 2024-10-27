f67fef9b5814da023cb47d24c71107ed09db28e8
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-10-27 20:25:07 +0530

    Develop ASP.Net Application through which user upload Image and that Image  should be displayed in Image Control.

---
+++ Journal 6/Journal6.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Journal6.aspx.cs" Inherits="Journal2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Image ID="Image1" runat="server" BorderStyle="Solid" BorderWidth="1px" Height="234px" Width="422px" />
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Upload" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Journal 6/Journal6.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Journal2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (FileUpload1.HasFile)
+        {
+            String fnm = FileUpload1.FileName;
+            String ext = System.IO.Path.GetExtension(fnm).ToString();
+            if (ext == ".jpg" || ext == ".jpeg" || ext == ".png" || ext == ".gif")
+            {
+                FileUpload1.SaveAs(Server.MapPath("~//uploads//") + fnm.ToString());
+                Image1.ImageUrl = ("~//uploads//") + fnm.ToString();
+                Label1.Text = "File Uploaded Successfully";
+            }
+            else
+                Label1.Text = "You have chosen Wrong File";
+        }
+        else
+            Label1.Text = "Please select File";
+    }
+}
