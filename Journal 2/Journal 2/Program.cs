1b5883d39f9e662f6b87a0dfd4fd39422a40d44e
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-10-27 20:00:42 +0530

    Write a program to demonstrate parameter passing mechanism and out parameter.

---
+++ Journal 2/Journal 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInputMultipleOutParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            GetUserInput(out i, out j);
+            Console.WriteLine("\nThe Addition of i is: " + i);
+            Console.WriteLine("The Addition of j is: " + j);
+            Console.Read();
+        }
+
+        public static void GetUserInput(out int i, out int j)
+        {
+            Console.Write("Enter a value for i: ");
+            i = int.Parse(Console.ReadLine()); 
+
+            Console.Write("Enter a value for j: ");
+            j = int.Parse(Console.ReadLine()); 
+
+            i += i; 
+            j += j; 
+        }
+    }
+}
