6cd90b9c206b410a3ff3083c39ca08f7f3e354d0
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-10-29 13:00:03 +0530

    Create application for displaying different reports.

---
+++ Journal 13/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<%@ Register assembly="CrystalDecisions.Web, Version=13.0.4000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" namespace="CrystalDecisions.Web" tagprefix="CR" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <CR:CrystalReportViewer ID="CrystalReportViewer1" runat="server" AutoDataBind="True" GroupTreeImagesFolderUrl="" Height="1202px" ReportSourceID="CrystalReportSource1" ToolbarImagesFolderUrl="" ToolPanelWidth="200px" Width="1104px" />
+        <CR:CrystalReportSource ID="CrystalReportSource1" runat="server">
+            <Report FileName="CrystalReport.rpt">
+            </Report>
+        </CR:CrystalReportSource>
+    
+    </div>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+    </form>
+</body>
+</html>
---
+++ Journal 13/Default.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Reporting;
+using CrystalDecisions.Shared;
+using System.IO;
+
+public partial class _Default : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=D:\Sanveda Dot Net Pratical\Journal 13\App_Data\Database.mdf;Integrated Security=True");
+    ReportDocument rprt = new ReportDocument();
+    string filepath;
+
+    public void pdfdemo()
+    {
+
+        Response.Clear();
+        filepath = Server.MapPath("~/" + "demo.pdf");
+        rprt.ExportToDisk(ExportFormatType.PortableDocFormat, filepath);
+        FileInfo fileinfo = new FileInfo(filepath);
+        Response.AddHeader("Content-Disposition", "inline;filenam=demo.pdf");
+        Response.ContentType = "application/pdf";
+        Response.WriteFile(fileinfo.FullName);
+
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        rprt.Load(Server.MapPath("~/CrystalReport.rpt"));
+        rprt.FileName = Server.MapPath("~/CrystalReport.rpt");
+        SqlCommand cmd = new SqlCommand("select * from Student", con);
+        SqlDataAdapter sda = new SqlDataAdapter(cmd);
+        DataSet ds = new DataSet();
+        sda.Fill(ds, "Student");
+        rprt.SetDataSource(ds);
+        CrystalReportViewer1.ReportSource = rprt;
+        CrystalReportViewer1.DataBind();
+        pdfdemo();
+    }
+}
